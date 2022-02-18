167a5a126d6385cc7c012101e3192aa18db8dd53
Yoonseo KIM <61778930+ottl-seo@users.noreply.github.com>
2022-02-18 19:06:48 +0900

    1003 피보나치 함수 김윤서

---
+++ 2월/3주차/1003_김윤서.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+pair<int, int> dp[10001] = {{0,0}, }; //0의 개수, 1의 개수
+int fibo(int n){
+	dp[0] = {1,0};
+	dp[1] = {0,1};
+	for(int i=1; i<n; i++){
+		dp[i+1].first = dp[i].first + dp[i-1].first;		
+		dp[i+1].second = dp[i].second + dp[i-1].second;		
+	}
+	return dp[n];
+}
+int main() {
+	int n;
+	cin>>n; 
+	for (int i = 0; i < n; i++) {
+			int t;
+			scanf("%d", &t); // t 입력받고
+			fibo(t);
+			printf("%d %d\n", dp[t].first, dp[t].second);
+		}
+}
